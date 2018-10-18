c01cc42b402b6ec5d7f4fdf41fc04b18aeabd7e8
Mariusz <mariusz.fb@gmail.com>
2018-10-18 14:03:27 +0100

    Created working MVC + testing html :: First pom.xml, basic file structure

---
+++ src/main/java/pl/xweb/blog/BlogMvcApp.java
@@ -0,0 +1,13 @@
+package pl.xweb.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMvcApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BlogMvcApp.class, args);
+    }
+}
